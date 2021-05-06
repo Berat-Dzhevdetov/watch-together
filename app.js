e6e80526590d5cb229334b9a2146b8de387ee8a9
Berat-Dzhevdetov <56674380+Berat-Dzhevdetov@users.noreply.github.com>
2021-05-06 14:16:45 +0300

    The app WON'T be single page application. Now it's changed to work with express server

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
 var express = require('express');
 var socket = require("socket.io");
 const mysql = require('mysql');
-
+var hbs = require('express-handlebars');
 
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 
-ytdl('http://www.youtube.com/watch?v=aqz-KE-bpKQ')
-    .pipe(fs.createWriteStream('./public/video.mp4'));
+// ytdl('http://rs
 
 //App setup
 var app = express();
 
 var server = app.listen(4000, function() {
-    console.log('listening to 4000 port');
-})
+        console.log('listening to 4000 port');
+    })
+    //Database connection
+let con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "watchtogether"
+});
 
-//Static files
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public/static'));
 
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/public/views/layouts/',
+    partialsDir: __dirname + '/public/views/partials/'
+}));
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res) {
+    res.render(__dirname + '\\public\\views\\home');
+});
 //Socket setup
 var io = socket(server);
 
